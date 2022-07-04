375f1112f7ef818946802a844a7506172246144c
prahuln7 <107910746+prahuln7@users.noreply.github.com>
2022-07-04 09:43:24 +0000

    Add files via upload

---
+++ TASK 444/Cosmos_Backup/output.tf
@@ -0,0 +1,6 @@
+output "azurerm_recovery_services_vault" {
+  value = azurerm_recovery_services_vault
+}
+output "azurerm_recovery_services_protection_policy_vm" {
+  value = azurerm_recovery_services_protection_policy_vm
+}
